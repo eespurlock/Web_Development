397e31decacf447a5e953d55f3f97da30542b0e8
eespurlock <eespurlock@uchicago.edu>
2020-02-26 10:49:48 -0600

    more

--- hw4/NASA.js
+++ hw4/NASA.js
@@ -1,35 +1,57 @@
+//API key: evUzRTFfYwSdaOpjdvSVIv4c0mv0Z9VsuIKibQEA
+//API example: https://api.nasa.gov/planetary/apod?api_key=evUzRTFfYwSdaOpjdvSVIv4c0mv0Z9VsuIKibQEA
+//astronomu picture of the day: https://apod.nasa.gov/apod/
+//https://apod.nasa.gov/apod/?api_key=evUzRTFfYwSdaOpjdvSVIv4c0mv0Z9VsuIKibQEA
+
 import React, { Component } from 'react';
 import './NASA.css';
 import jQuery;
 
 class Asteroid extends Component {
+
+	constructor(props){
+		constructor(props){
+			super(props)
+			this.state ={this_asteroid: null,
+				this_picture = 'https://apod.nasa.gov/apod/image/2002/MoonHalo_Mckean_5612.jpg'}
+		}
+	} 
 	
-	constructor(){//I'm not 100% sure what is supposed to be here	
-	}
-	 render(){
+	render(){
 		 return(
 		<div>
 			<button onClick={this.handleRefresh}>View Asteroids</button>
 			//In here, I also need to get the APOD
+			<img src={this.props.this_picture}/>
 		</div>
 		 )
 	 }
 	 
 	 handleRefresh = (e) =>{
+		 
+		 //first, I need to find the date
+		 
 		 e.preventDefault()
-		 const url_asteroid = this.urlForAsteroids()
-		 const json_picture = 'https://api.nasa.gov/planetary/apod?api_key=evUzRTFfYwSdaOpjdvSVIv4c0mv0Z9VsuIKibQEA'
-		 //get the JSON from this url
-		 .getJSON(json_picture, function(apod){})
-		 //get the "url" for this picture from the JSON
-		 const apod = json_picture.parse(data)
-		 const apod = apod["url"]
+		 const url_asteroid = this.urlForAsteroids(start_date)
+		 const picture_url = 'https://api.nasa.gov/planetary/apod?api_key=evUzRTFfYwSdaOpjdvSVIv4c0mv0Z9VsuIKibQEA'
+		
+		//refreshes the picture
+		fetch(picture_url).then((p) => p.json).then((data) => {
+			console.log(data)
+			this.setState({this_picture: data.results.hdurl})
+		 });
+		 
+		 fetch(picture_url).then((p) => p.json).then((data) => {
+				console.log(data)
+				this.setState({this_picture: data.results})
+			 });
 	 }
 	 
-	 urlForAsteroids = () => {
+	 urlForAsteroids = (start_date) => {
 		 const api_key = "api_key=evUzRTFfYwSdaOpjdvSVIv4c0mv0Z9VsuIKibQEA"
-		 const base_url = "https://api.nasa.gov/api.html#NeoWS"
-		 const api_url = "${base_url}?${api_key}"
+		 const base_url = "https://api.nasa.gov/neo/rest/v1/feed?"
+		 const start_date = "start_date=" + start_date
+		 const api_url = "${base_url}?${start_date}?${api_key}"
 		 return api_url
 	 }
 	
